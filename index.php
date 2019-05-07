50aa10c184d895d56cca5ce48fd931fc3372004e
jhon martinez <jhonmartinez.exec@gmail.com>
2019-05-07 12:39:19 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="_includes/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="_includes/style.css">
+<script src="_includes/jquery-3.4.0.js"></script>
+<script src="_includes/bootstrap.js"></script>
+<head>
+	<title>The Hero Association</title>
+</head>
+<body>
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+		<a class="navbar-brand" href="index.php"><img src="_images/ha-logo.png" width="40" height="40" alt=""></a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Heroes</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Monsters</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Memes</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Account
+        </a>
+        <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item text-light" href="#">Login</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item text-light" href="#">Register</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+<!--   <ul class="navbar-nav">
+  	<li class="navbar-item">
+        <a class="nav-link" href="#"><button class="btn btn-dark login">Login</button></a>
+     </li>
+     <li class="navbar-item">
+        <a class="nav-link" href="#"><button class="btn btn-dark login">Register</button></a>
+     </li>
+   </ul> -->
+	</nav>
+
+</body>
+</html>
