e613c26753df722a647f1c533353679446962a57
Escanor <escanor0116@gmail.com>
2019-05-09 11:28:02 +0800

    hero profile

--- heroProfile.php
+++ heroProfile.php
@@ -1,16 +1,25 @@
 <?php  
 	include_once 'homepageHeader.php';
+	include_once './_includes/config.php';
+	session_start();
+	$hero_id = $_SESSION['heroID'];
+	$heroQuery = "SELECT hero_rank_no,hero_class, hero_firstname,hero_lastname,hero_fighting_style,hero_city,hero_username FROM heroRankings INNER JOIN heroProfiles ON heroRankings.hero_user_id = heroProfiles.hero_user_id WHERE heroRankings.hero_user_id = '".$hero_id."';";
+	$data = selectRanking($heroQuery);
 ?>
 <div class="container">
 	<div class="profile">
 		<img src="_images/genos.png" class="heroAvatar">
 		<div class="heroInfo">
-			<h3 class="heroName">Full Name: Genos</h3>
-			<h3>Hero Name: Demon Cyborg</h3>
-			<h3>Class: S</h3>
-			<h3>Rank: 14</h3>
-			<h3>City: Z</h3>
-			<h3>Fighting Style: Unknown</h3>
+			<?php 
+			foreach ($data as $heroInfo):
+			?>
+
+			<h3 class="heroName">Fullname: <?php echo $heroInfo['hero_firstname'], " ",echo $heroInfo['hero_lastname'];  ?></h3>
+			<h3>Hero Name: <?php echo $heroInfo['hero_username']; ?></h3>
+			<h3>Class: <?php echo $heroInfo['hero_class']; ?></h3>
+			<h3>Rank: <?php echo $heroInfo['hero_rank_no']; ?></h3>
+			<h3>City: <?php echo $heroInfo['hero_city']; ?></h3>
+			<h3>Fighting Style: <?php echo $heroInfo['hero_fighting_style']; ?></h3>
 		</div>
 	</div>
 </div>
