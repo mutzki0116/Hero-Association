ab6763eb877cd46bd3a9d78a159b0840e1c87ce9
Escanor <escanor0116@gmail.com>
2019-05-09 10:19:24 +0800

    Inner join on Rankings Five

--- heroRankings.php
+++ heroRankings.php
@@ -2,7 +2,7 @@
 	include_once 'homepageHeader.php';
 	include_once './_includes/config.php';
 
-	$heroQuery = "SELECT hero_firstname, hero_lastname, hero_class, hero_rank_no FROM heroProfiles INNER JOIN heroRankings ON heroProfiles.hero_user_id = heroRankings.hero_user_id WHERE heroRankings.hero_class == 'C'";
+	$heroQuery = "SELECT hero_firstname, hero_lastname, hero_class, hero_rank_no FROM heroProfiles INNER JOIN heroRankings ON heroProfiles.hero_user_id = heroRankings.hero_user_id WHERE heroRankings.hero_class == C";
 	$pdo = getConnection();
 	$heroResult = $pdo->prepare($heroQuery);
 	$heroResult->execute();
