63d1ab40a481b4f50bcd1fab3c99b3ca44e320fa
Escanor <escanor0116@gmail.com>
2019-05-09 10:47:57 +0800

    select ranking 4

--- heroRankings.php
+++ heroRankings.php
@@ -3,7 +3,7 @@
 	include_once './_includes/config.php';
 
 	$classc = 'c'
-	$heroQuery = "SELECT * FROM heroRankings WHERE hero_class = '".$classc."'";
+	$heroQuery = "SELECT * FROM heroRankings WHERE hero_class;";
 	$data = selectRanking($heroQuery);
 ?>
 <h1>Rankings</h1>
