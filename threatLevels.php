402d7ff9ee9c3acc707538f47278b371aa5f2606
jhon martinez <jhonmartinez.exec@gmail.com>
2019-05-10 13:49:20 +0800

    threat levels

---
+++ threatLevels.php
@@ -0,0 +1,58 @@
+<?php  
+	include_once 'adminHeader.php';
+?>
+
+<h1 class="pageTitle">Threat Levels</h1>
+<div class="container">
+	<div class="row">	
+		<div class="col-xl-4 col-md-6">
+			<div class="card monsterContainer">
+			<img class="card-img-top monster-img" src="_images/levels/god.jpg">
+			  	<div class="card-body">
+				    <h2 class="card-title">GOD</h2>
+				    <p class="card-text">Threat to the planet itself</p>
+				    <hr>
+				    <a href="#">READ MORE</a>
+			  	</div>
+			</div>
+		</div>
+		
+		<div class="col-xl-4 col-md-6">
+			<div class="card monsterContainer">
+			<img class="card-img-top monster-img" src="_images/levels/dragon.jpg">
+			  	<div class="card-body">
+				    <h2 class="card-title">DRAGON</h2>
+				    <p class="card-text">Threat to entire countries</p>
+				    <hr>
+				    <a href="#">READ MORE</a>
+			  	</div>
+			</div>
+		</div>
+
+		<div class="col-xl-4 col-md-6">
+			<div class="card monsterContainer">
+			<img class="card-img-top monster-img" src="_images/levels/demon.jpg">
+			  	<div class="card-body">
+				    <h2 class="card-title">DEMON</h2>
+				    <p class="card-text">Threat to an entire city</p>
+				    <hr>
+				    <a href="#">READ MORE</a>
+			  	</div>
+			</div>
+		</div>
+
+		<div class="col-xl-4 col-md-6">
+			<div class="card monsterContainer">
+			<img class="card-img-top monster-img" src="_images/levels/tiger.jpg">
+			  	<div class="card-body">
+				    <h2 class="card-title">TIGER</h2>
+				    <p class="card-text">Threat to a small town</p>
+				    <hr>
+				    <a href="#">READ MORE</a>
+			  	</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
