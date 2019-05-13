1d961a8a2dc997ca360fd8a804691615a726214c
jhon martinez <jhonmartinez.exec@gmail.com>
2019-05-13 15:49:42 +0800

    addMonster

--- addMonster.php
+++ addMonster.php
@@ -8,7 +8,8 @@
 			$mthreat = $_POST['threat'];
 			$mimg = $_POST['img'];
 			$mstatus = $_POST['stats'];
-			$monsterQuery = "INSERT INTO monsterList(monster_name, monster_description, monster_image, monster_threat_lvl, moster_status,date_added) VALUES(:mname,:mdesc,:mimg,:mthreat,:mstatus);";
+			$date = $_POST['date_added'];
+			$monsterQuery = "INSERT INTO monsterList(monster_name, monster_description, monster_image, monster_threat_lvl, moster_status,date_added) VALUES(:mname,:mdesc,:mimg,:mthreat,:mstatus,:date_added);";
 			$data = addMonsters($monsterQuery, [
 			['monster' => ':mname', 'value' => $mname],
 			['monster' => ':mdesc', 'value' => $mdesc],
@@ -16,6 +17,7 @@
 			['monster' => ':mthreat', 'value' => $mthreat],
 			['monster' => ':mstatus', 'value' => $mstatus],
 			['monster' => ':mstatus', 'value' => $mstatus],
+			['monster' => ':date_added', 'value' => $date],
 			]);
 			header("Location: monsters.php");
 		}
@@ -59,7 +61,10 @@
 			<label>Monster Description </label>
 			<textarea class="form-control" rows="5" placeholder="Description" name="desc" required></textarea>
 			<input type="submit" name="addMonster" class="btn btn-outline-primary addMonsterBtn" value="Submit">
-	
+		<?php 
+		$date = date("Y-m-d H:i:s");
+		echo '<input type="text" class="form-control" name="date_added" value="'.$date.'" style="display:none">';
+		?>
 	</div>
 </form>
 </div>
