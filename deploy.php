8df77e6952c9d99ac865775d9bf36a37d863d1e3
Escanor <escanor0116@gmail.com>
2019-05-15 13:26:38 +0800

    Echo out the data in task name selected option

--- deploy.php
+++ deploy.php
@@ -24,8 +24,8 @@
 		<div class="col-xl-4">
 			<label>Hero </label>
 			<select class="form-control">
-				<?php foreach ($data3 as $task): ?>
-				<option><?php echo $task['task_name']; ?></option>
+				<?php foreach ($data2 as $hero): ?>
+				<option><?php echo $hero['hero_username']; ?></option>
 				<?php endforeach; ?>
 			</select>
 		</div>
@@ -34,8 +34,9 @@
 		<div class="col-xl-8">
 			<label>Task</label>
 			<select class="form-control">
-				<option>Kill</option>
-				<option>Capture</option>
+				<?php foreach ($data3 as $task): ?>
+				<option><?php echo $task['task_name']; ?></option>
+				<?php endforeach; ?>
 			</select>
 		</div>
 
