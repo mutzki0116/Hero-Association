168a2b895260852d3d2ef7cd3e71006a6c0f6baa
jhon martinez <jhonmartinez.exec@gmail.com>
2019-05-16 16:32:45 +0800

    homepage

--- homepage.php
+++ homepage.php
@@ -2,16 +2,18 @@
 	include_once 'homepageHeader.php';
 	// $id = $_SESSION['heroID'];
 ?>
-<h1>Homepage</h1>
 	<!-- <h2><?php echo $_SESSION['heroID']; ?></h2> -->
 
-	<div id="carouselExampleIndicators" class="carousel slide announcement" data-ride="carousel">
+<!-- 	<div id="carouselExampleIndicators" class="carousel slide announcement" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
   </ol>
+
+  <div class="homapageDesign">
+<h2>Homapage</h2>
   <div class="carousel-inner">
     <div class="carousel-item active">
       <img class="d-block w-100" src="_images/levels/god.jpg">
@@ -26,6 +28,9 @@
       <img class="d-block w-100" src="_images/levels/tiger.jpg">
     </div>
   </div>
+
+  </div>
+
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
@@ -34,6 +39,6 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
-</div>
+</div> -->
 </body>
 </html>
