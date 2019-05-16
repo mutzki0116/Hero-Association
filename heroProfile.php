f75c68d8ecec49e3d3466cb254aff4defaf862a4
jhon martinez <jhonmartinez.exec@gmail.com>
2019-05-16 14:40:50 +0800

    profile design

--- heroProfile.php
+++ heroProfile.php
@@ -1,11 +1,10 @@
 <?php  
 	include_once 'homepageHeader.php';
 	include_once './_includes/config.php';
-	session_start();
 	
 		if(isset($_SESSION['heroID'])){
 			$hero_id = $_SESSION['heroID'];
-			$heroQuery = "SELECT hero_rank_no,hero_class, hero_firstname,hero_lastname,hero_fighting_style,hero_city,hero_username FROM heroRankings INNER JOIN heroProfiles ON heroRankings.hero_user_id = heroProfiles.hero_user_id WHERE heroRankings.hero_user_id = '".$hero_id."';";
+			$heroQuery = "SELECT hero_rank_no,hero_class, hero_firstname,hero_lastname,hero_ext_name,hero_fighting_style,hero_city,hero_username FROM heroRankings INNER JOIN heroProfiles ON heroRankings.hero_user_id = heroProfiles.hero_user_id WHERE heroRankings.hero_user_id = '".$hero_id."';";
 			$data = selectRanking($heroQuery);
 		}
 		else{
@@ -13,23 +12,22 @@
 		}
 
 ?>
-<div class="container">
 	<div class="profile">
 		<img src="_images/genos.png" class="heroAvatar">
 		<div class="heroInfo">
 			<?php 
 			foreach ($data as $heroInfo):
 			?>
-
-			<h3 class="heroName">Fullname: <?php echo $heroInfo['hero_firstname']; ?></h3>
-			<h3>Hero Name: <?php echo $heroInfo['hero_username']; ?></h3>
-			<h3>Class: <?php echo $heroInfo['hero_class']; ?></h3>
-			<h3>Rank: <?php echo $heroInfo['hero_rank_no']; ?></h3>
-			<h3>City: <?php echo $heroInfo['hero_city']; ?></h3>
-			<h3>Fighting Style: <?php echo $heroInfo['hero_fighting_style']; ?></h3>
+			<div class="container">
+			<h3 class="heroName"><?php echo $heroInfo['hero_firstname'].' '.$heroInfo['hero_lastname'].' '.$heroInfo['hero_ext_name']; ?></h3>
+			<h3 class="heroSubInfo">Hero Name: <?php echo $heroInfo['hero_username']; ?></h3>
+			<h3 class="heroSubInfo">Class: <?php echo $heroInfo['hero_class']; ?></h3>
+			<h3 class="heroSubInfo">Rank: <?php echo $heroInfo['hero_rank_no']; ?></h3>
+			<h3 class="heroSubInfo">City: <?php echo $heroInfo['hero_city']; ?></h3>
+			<h3 class="heroSubInfo">Fighting Style: <?php echo $heroInfo['hero_fighting_style']; ?></h3>
+			</div>
 		</div>
 	<?php endforeach; ?>
 	</div>
-</div>
 </body>
 </html>
